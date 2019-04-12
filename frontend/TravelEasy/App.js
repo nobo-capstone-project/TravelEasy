53c6eb9d0a043e88f29f6fe3d769dbc02006ef4b
s-rjk <robjkim@uw.edu>
2019-04-11 23:32:30 -0700

    home page in progress

--- frontend/TravelEasy/App.js
+++ frontend/TravelEasy/App.js
@@ -7,58 +7,114 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+// import { Text, View } from 'react-native';
 
 
-// import { Container, Header, Content, Tab, Tabs } from 'native-base';
-import { Tab, Tabs } from 'native-base';
-import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
+import { Card, CardItem, Container, Header, Content, Tab, Tabs, FooterTab, Footer, Button, Icon, } from 'native-base';
+// import { Tab, Tabs } from 'native-base';
+// import { Container, Header, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+// import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
+
+
+const styles = StyleSheet.create({
+  container: {
+    // paddingTop: 200
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0
+  },
+  guideContainer: {
+    backgroundColor: 'blue',
+    height: 100,
+    // width: 100,
+    // position: 'absolute',
+    // top: 100,
+    // left: 0
+  },
+  tabs: {
+    // marginTop: 0,
+    backgroundColor: 'red',
+    height: 50
+
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <Container>
+      <Container style={styles.container}>
+        {/* <View><Text>Hello</Text></View> */}
+        {/* <View></View> */}
+        <Text> Travel</Text>
+
         <Header hasTabs />
-        <Tabs>
+
+        <Tabs style={styles.tabs}>
           <Tab heading="All">
             {/* <Tab1 /> */}
           </Tab>
           <Tab heading="Trending">
             {/* <Tab2 /> */}
           </Tab>
-          <Tab heading="Favorites">
-            {/* <Tab3 /> */}
-          </Tab>
+
         </Tabs>
 
-        <Footer>
+        <Card>
+
+          <CardItem><Text>hsdfas</Text></CardItem>
+          <CardItem><Text>hsdfas</Text></CardItem>
+          <CardItem><Text>hsdfas</Text></CardItem>
+          <CardItem><Text>hsdfas</Text></CardItem>
+          <CardItem><Text>hsdfas</Text></CardItem>
+          <CardItem><Text>hsdfas</Text></CardItem>
+          <CardItem><Text>hsdfas</Text></CardItem>
+        </Card>
+
+        <Content style={styles.guideContainer} ref={c => (this.component = c)}>
+          <Text style={{ margin: 20 }}>test</Text>
+          <Text style={{ margin: 20 }}>test</Text>
+          <Text style={{ margin: 20 }}>test</Text>
+          <Text style={{ margin: 20 }}>test</Text>
+          <Text style={{ margin: 20 }}>test</Text>
+          <Text style={{ margin: 20 }}>test</Text><Text style={{ margin: 20 }}>test</Text>
+          <Text style={{ margin: 20 }}>test</Text>
+          <Text style={{ margin: 20 }}>test</Text>
+        </Content>
+
+
+
+
+        <Footer style={styles.footer}>
           <FooterTab>
             <Button vertical>
               <Icon name="apps" />
-              <Text>Apps</Text>
+              <Text>Explore</Text>
             </Button>
             <Button vertical>
               <Icon name="camera" />
-              <Text>Camera</Text>
+              <Text>Favorites</Text>
             </Button>
             <Button vertical active>
               <Icon active name="navigate" />
-              <Text>Navigate</Text>
+              <Text>Trips</Text>
             </Button>
             <Button vertical>
               <Icon name="person" />
-              <Text>Contact</Text>
+              <Text>Add</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="person" />
+              <Text>User</Text>
             </Button>
           </FooterTab>
         </Footer>
-      </Container>
+
+      </Container >
     );
   }
 }
